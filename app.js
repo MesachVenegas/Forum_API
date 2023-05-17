bd39dc198bce9e59be0cedba280e520d375cb769
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-17 12:31:02 -0700

    create: server base

---
+++ app.js
@@ -0,0 +1,31 @@
+const initModel = require('./src/models/init.models');
+const db = require('./src/config/connection');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+initModel();
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+db.authenticate()
+    .then(() => console.log("Conectado a la base de datos"))
+    .catch(err => console.error(err))
+
+db.sync({ force: true })
+    .then(() => console.log("Base de datos Sincronizada correctamente"))
+    .catch(err => console.error(err));
+
+app.get('/', (req, res) =>{
+    console.log("Servidor en linea");
+    res.send("Servidor en linea");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server ON, listen on port ${PORT}`)
+});
+
