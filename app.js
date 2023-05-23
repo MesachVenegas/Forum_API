63bdafc3083a3a0c771370c6dbe5d28bf1a63284
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-23 08:46:55 -0700

    change: remove alter table apply force to reset DDBB and return normal

--- app.js
+++ app.js
@@ -21,49 +21,13 @@ db.authenticate()
     .then(() => console.log("Conectado a la base de datos"))
     .catch(err => console.error(err))
 
-db.sync({ alter: true })
+db.sync({ force: true })
     .then(() => console.log("Base de datos Sincronizada correctamente"))
     .catch(err => console.error(err));
 
 app.get('/', (req, res) =>{
     console.log("Servidor en linea");
     res.send("Servidor en linea");
-})
-
-// Obtener un Post con su categoria y el usuario que la creo.
-app.get('/posts/:id', async (req, res) =>{
-    try {
-        //  Si se uniran 2 tablas o mas se require incluir en el include un array de modelos a incluir
-        const { id } = req.params;
-        const post = await Posts.findByPk(id, {
-            attributes: {
-                exclude: ['user_id', 'userId', 'categoryId', 'category_id'],
-            },
-            include: [
-                {
-                    model: Users,
-                    attributes: ['id', 'user_name']
-                },
-                {
-                    model: Categories,
-                    attributes: ['id', 'category' ]
-                },
-                {
-                    model: Answers,
-                    include: [
-                        {
-                            model: Users,
-                            attributes: ['id', 'user_name']
-                        }
-                    ]
-                }
-            ]
-        })
-        res.json(post)
-    } catch (error) {
-        console.error(error);
-        res.send(error.message);
-    }
 })
 
 app.listen(PORT, () => {
