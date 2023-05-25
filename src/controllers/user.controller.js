893f6b77e308fda25e8c73a88983968f16e6a703
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-25 00:11:11 -0700

    change: remove validations on user services

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,37 +4,13 @@ const bcrypt = require('bcrypt');
 const createNerUser = async (req, res) => {
     try {
         // ! validar que los campos obligatorios estén presentes.
-        const { username, email, password , rolId} = req.body;
-        // * Asegurarse que no venga vacía y que sea el tipo de dato requerido.
-        if(typeof(username) !== 'string' || !username){
-            return res.status(400).json({
-                error: 'Invalid username',
-                errorMessage: 'Username must be a string and cannot be empty.'
-            })
-        }
-        if(typeof(email) !== 'string' || !email){
-            return res.status(400).json({
-                error: 'Invalid email',
-                errorMessage: 'email must be a string and cannot be empty.'
-            })
-        }
-        if(typeof(password) !== 'string' || !password){
-            return res.status(400).json({
-                error: 'Invalid password',
-                errorMessage: 'password must be a string and cannot be empty.'
-            })
-        }
+        const { username, email, password, rolId } = req.body;
 
         // * Hashed de contraseña con Bcrypt.
         const hashed = await bcrypt.hash(password, 10);
 
         // * Si todo sale bien creamos el usario.
-        await UserService.create({
-            username: username,
-            email: email,
-            password: hashed,
-            rolId: rolId
-        })
+        await UserService.create({ username, email, password: hashed, rolId })
         res.status(201).send();
     } catch (error) {
         res.status(400).json(error);
