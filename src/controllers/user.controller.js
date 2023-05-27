d3dbf1cd53412ac6b8e707996ea52fe35f9a5162
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-27 07:34:17 -0700

    typo: remove comments

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,11 @@
 const UserService = require('../services/user.service');
 const bcrypt = require('bcrypt');
 
-const createNerUser = async (req, res) => {
+const createNerUser = async (req, res, next) => {
     try {
-        // ! validar que los campos obligatorios estén presentes.
         const { username, email, password, rolId } = req.body;
-
         // * Hashed de contraseña con Bcrypt.
         const hashed = await bcrypt.hash(password, 10);
-
         // * Si todo sale bien creamos el usario.
         await UserService.create({ username, email, password: hashed, rolId })
         res.status(201).send();
@@ -18,7 +15,7 @@ const createNerUser = async (req, res) => {
 
 }
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = async (req, res, next) => {
     try {
         const response = await UserService.getUsers();
         res.status(200).json(response)
@@ -27,17 +24,17 @@ const getAllUsers = async (req, res) => {
     }
 }
 
-
-const userLogin = async (req, res) => {
+const userLogin = async (req, res, next) => {
     try {
         const { email, password } = req.body;
-        const response = await UserService.login(email, password);
+        const user = await UserService.finduser(email, next)
+        console.log(user)
+        // const response = await UserService.login(email, password, next);
         res.status(200).json(response);
     } catch (error) {
         res.status(400).json(error);
     }
 }
-
 
 module.exports = {
     createNerUser,
